9972196e380054d3ae0def89b1c1b33340733f51
treebirg1 <treebirg@gmail.com>
2013-08-24 16:41:10 +0300

    First commit of node.js REST API

--- modules/api.js
+++ modules/api.js
@@ -2,14 +2,21 @@ var crypto = require('crypto');
 
 var app;
 
+exports.register = function(params,cb){
+	
+};
+
 exports.tryLogin = function(params,cb){
+	console.log('inside tryLogin!');
     var hash_pass = crypto.createHash('sha256').update(params.password).digest('hex');
+	console.log('hash pass: ' + hash_pass);
 	db = params.db;
 	db.collection('users').findOne({username:params.mail,password:hash_pass},function(err,user_doc){
 		if(err)throw err;
 		if(user_doc){
-			if(typeof cb === 'function') cb(true)
-			else cb(false);
-		}
+			console.log('Found user! Yay');
+			if (cb && typeof(cb) === "function") cb(true); 
+		}else
+			if (cb && typeof(cb) === "function") cb(false);
 	});
 };
--- routes/route_api.js
+++ routes/route_api.js
@@ -1,21 +1,21 @@
+var API = require('./../modules/api.js');
+var url = require('url');
+
 module.exports = function(db,app){
-	app.get('/api/login?:mail&.:password', function(req,res){
-		var params ={};
-		params.mail = req.params.username;
-		params.password = req.params.password;
+	app.get('/api/login', function(req,res){
+		var url_parts = url.parse(req.url,true);
+		console.log(url_parts.query);
+		var params = url_parts.query;
 		params.db = db;
 		API.tryLogin(params,function(valid){
-			if(valid){ 
-				console.log('yeah it works!');
-				res.send(200);
+			if(valid){
+				console.log('yeah he\'s legit! let him pass!');
 			}
 			else
-			{
-				console.log('nope not today!');
-				res.send(300);
-			}
+				console.log('nope ... never seen him!');
 		});
 	});
+	app.post('/api/register'
 	app.post('/api/test', function(req,res){
 		console.log('posted data to test');
 		console.log(req.body);
